ad868407888115580038da443148b691d08022e1
OtaOkpogo <otaokpogo@yahoo.com>
2024-05-28 20:17:55 -0700

    Updates

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """
 Contains the class DBStorage
@@ -74,3 +75,32 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        '''get:
+        retrieve an object from the file storage by class and id.
+        '''
+        if cls in classes.values() and id and type(id) == str:
+            d_obj = self.all(cls)
+            for key, value in d_obj.items():
+                if key.split(".")[1] == id:
+                    return value
+        """
+        Returns the object based on the class name and its ID, or None if not
+        found
+        """
+        objects = self.__session.query(classes[cls])
+        for obj in objects:
+            if obj.id == id:
+                return obj
+        return None
+
+    def count(self, cls=None):
+        '''count:
+        count the number of objects in storage matching the given class.
+        '''
+        data = self.all(cls)
+        if cls in classes.values():
+            data = self.all(cls)
+        return len(data)
+
