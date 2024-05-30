b365798d69d295ef46a6347bc0a8796e20202323
OtaOkpogo <otaokpogo@yahoo.com>
2024-05-29 20:06:24 -0700

    updates

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -46,38 +46,50 @@ def del_city(city_id):
     return jsonify({})
 
 
-@app_views.route('/states/<string:state_id>/cities', methods=['POST'],
-                 strict_slashes=False)
+@app_views.route('/states/<string:state_id>/cities', methods=['POST'], strict_slashes=False)
 @swag_from('documentation/city/post.yml', methods=['POST'])
 def create_obj_city(state_id):
     """ create new instance """
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
-    if not request.get_json():
+    
+    if not request.is_json:
         return make_response(jsonify({"error": "Not a JSON"}), 400)
-    if 'name' not in request.get_json():
+    
+    try:
+        data = request.get_json(force=True)
+    except Exception:
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
+    
+    if 'name' not in data:
         return make_response(jsonify({"error": "Missing name"}), 400)
-
-    js = request.get_json()
-    obj = City(**js)
+    
+    obj = City(**data)
     obj.state_id = state.id
     obj.save()
     return jsonify(obj.to_dict()), 201
 
 
-@app_views.route('/cities/<string:city_id>', methods=['PUT'],
-                 strict_slashes=False)
+@app_views.route('/cities/<string:city_id>', methods=['PUT'], strict_slashes=False)
 @swag_from('documentation/city/put.yml', methods=['PUT'])
 def post_city(city_id):
-    """  """
-    if not request.get_json():
+    """ update city by id """
+    if not request.is_json:
         return make_response(jsonify({"error": "Not a JSON"}), 400)
+    
+    try:
+        data = request.get_json(force=True)
+    except Exception:
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
+    
     obj = storage.get(City, city_id)
     if obj is None:
         abort(404)
-    for key, value in request.get_json().items():
+    
+    for key, value in data.items():
         if key not in ['id', 'state_id', 'created_at', 'updated_at']:
             setattr(obj, key, value)
-    storage.save()
+    obj.save()
     return jsonify(obj.to_dict())
+
